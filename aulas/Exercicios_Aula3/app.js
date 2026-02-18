6c189a5cc2a5416a3f03d71b6ee0896011ddaee1
BielVereda <gabrielsantosvereda@gmail.com>
2026-02-18 10:27:04 -0300

    feat: Adicionar arquivos app.js e index.html com exemplos de variáveis e operações matemáticas

---
+++ aulas/Exercicios_Aula3/app.js
@@ -0,0 +1,13 @@
+let nome = "Gabriel dos Santos Vereda";
+let idade = 18;
+let cidade = "SP";
+
+let num1 = 1;
+let num2 = 3;
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos e moro em ${cidade}.`);
+
+console.log(`Soma: ${num1} + ${num2} = ${num1+num2}`);
+console.log(`Subtração: ${num1} - ${num2} = ${num1-num2}`);
+console.log(`Multiplicação: ${num1} X ${num2} = ${num1*num2}`);
+console.log(`Divisão: ${num1} / ${num2} = ${num1/num2}`);
